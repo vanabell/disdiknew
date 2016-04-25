60e6cd3b7c33c21d3fbce9366aa34624edaf75da
Reza Fauzan <reza@icube.us>
2016-04-25 13:41:24 +0700

    fix edit

---
+++ application/controllers/FasilitasController.php
@@ -0,0 +1,36 @@
+<?php
+
+class FasilitasController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function programAction()
+    {
+        // action body
+    }
+
+    public function saranaAction()
+    {
+        // action body
+    }
+
+    public function mitraAction()
+    {
+        // action body
+    }
+
+    public function asetAction()
+    {
+        // action body
+    }
+
+}
