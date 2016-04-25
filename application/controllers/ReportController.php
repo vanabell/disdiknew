48b793a0ca7478263aac44738373cfc42f3ad45a
Reza Fauzan <reza@icube.us>
2016-04-25 22:46:51 +0700

    fix all

---
+++ application/controllers/ReportController.php
@@ -0,0 +1,30 @@
+<?php
+
+class ReportController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function trainingAction()
+    {
+        // action body
+    }
+
+    public function certificateAction()
+    {
+        // action body
+    }
+
+    public function certificationAction()
+    {
+        // action body
+    }
+}
