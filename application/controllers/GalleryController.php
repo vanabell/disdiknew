24be723e320e1d96d025729a91b46d0457ed828e
Reza Fauzan <reza@icube.us>
2016-04-25 21:41:18 +0700

    slider and all

---
+++ application/controllers/GalleryController.php
@@ -0,0 +1,24 @@
+<?php
+class GalleryController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function photoAction()
+    {
+        // action body
+    }
+
+    public function videoAction()
+    {
+        // action body
+    }
+}
--- application/controllers/NewsController.php
+++ application/controllers/NewsController.php
@@ -3,25 +3,42 @@
 class NewsController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
+   public function init()
+   {
+       /* Initialize action controller here */
+   }
 
-    public function indexAction()
-    {
-        $model = new Application_Model_NewsModel();
-        $data = $model->getAllNews();
-        $this->view->data = $data;
-    }
+   public function indexAction()
+   {
+       $model = new Application_Model_NewsModel();
+       $data = $model->getAllNews();
+       foreach ($data as $news) {
+           $truncated = $this->truncateIfNecessary($news['isi'],150);
+           $newsdata[] =  array('id_berita' => $news['id_berita'],'judul'=>$news['judul'], 'isi'=>$truncated,'gambar'=>$news['gambar'],'waktu'=>$news['waktu'],'edit_time'=>$news['edit_time'] );
+       }
+       $this->view->data = $newsdata;
 
-    public function detailAction()
-    {
-        $model = new Application_Model_NewsModel();
-        $req = $this->getRequest();
-        $id = $req->getParam('p');
-        $getnews = $model->getAllNewsDet($id);
-        $this->view->data = $getnews;
-    }
+   }
+
+   public function detailAction()
+   {
+       $model = new Application_Model_NewsModel();
+       $req = $this->getRequest();
+       $id = $req->getParam('p');
+       $getnews = $model->getAllNewsDet($id);
+       $this->view->data = $getnews;
+   }
+
+
+   function truncateIfNecessary($string, $length) {
+      if(strlen($string) > $length) {
+          $string = html_entity_decode(strip_tags($string));
+          $string = substr($string, 0, $length).'...';
+          $string = htmlentities($string);
+          return $string;
+      } else {
+          return strip_tags($string);
+      }
+   }
 
 }
