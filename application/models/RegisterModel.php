0a80f23e856816712c6df94ec822b85d5410d65b
HendyIvan <ivan@icube.us>
2016-06-19 13:55:56 +0700

    add save guru

---
+++ application/models/RegisterModel.php
@@ -0,0 +1,34 @@
+<?php
+class Application_Model_RegisterModel {
+
+	protected $_dbTableProduct;
+	protected $_db;
+
+	public function __construct()
+	{
+		$this->_dbTableProduct = new Application_Model_DbTables_RegisterModel();
+		$this->_db = Zend_Registry::get('db_doc');
+	}
+
+	public function insertGuru($data, $password) {
+		$productTable = $this->_dbTableProduct;
+		try {
+			$result = $productTable->insertGuru($data, $password);
+
+		} catch (Zend_Exception $e) {
+			return $e->getMessage();
+		}
+		return $result;
+	}
+
+	public function cekEmailGuru($data,$nip) {
+		$productTable = $this->_dbTableProduct;
+		try {
+			$result = $productTable->cekEmailGuru($data,$nip);
+
+		} catch (Zend_Exception $e) {
+			return $e->getMessage();
+		}
+		return $result;
+	}
+}
