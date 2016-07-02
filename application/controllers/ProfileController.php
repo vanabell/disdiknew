453277245121be29b56aecfd4013cb3b6de26569
Reza Fauzan <reza@icube.us>
2016-07-02 21:34:58 +0700

    add page fasilitas n input data

--- application/controllers/ProfileController.php
+++ application/controllers/ProfileController.php
@@ -10,7 +10,6 @@ class ProfileController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
         $model = new Application_Model_ProfileModel();
         $data = $model->getAllProfileDet(1);
         $this->view->data = $data;
@@ -18,7 +17,6 @@ class ProfileController extends Zend_Controller_Action
 
     public function historyAction()
     {
-        // action body
         $model = new Application_Model_ProfileModel();
         $data = $model->getAllProfileDet(2);
         $this->view->data = $data;
@@ -26,7 +24,6 @@ class ProfileController extends Zend_Controller_Action
 
     public function jobdeskAction()
     {
-        // action body
         $model = new Application_Model_ProfileModel();
         $data = $model->getAllProfileDet(3);
         $this->view->data = $data;
@@ -34,7 +31,6 @@ class ProfileController extends Zend_Controller_Action
 
     public function structureAction()
     {
-        // action body
         $model = new Application_Model_ProfileModel();
         $data = $model->getAllProfileDet(4);
         $this->view->data = $data;
@@ -42,7 +38,6 @@ class ProfileController extends Zend_Controller_Action
 
     public function institutionAction()
     {
-        // action body
         $model = new Application_Model_ProfileModel();
         $data = $model->getAllProfileDet(5);
         $this->view->data = $data;
@@ -50,7 +45,6 @@ class ProfileController extends Zend_Controller_Action
 
     public function visimisiAction()
     {
-        // action body
         $model = new Application_Model_ProfileModel();
         $data = $model->getAllProfileDet(6);
         $this->view->data = $data;
