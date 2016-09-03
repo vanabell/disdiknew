d340b506f5acf7c5f4ebe26ab6199e9859c880e5
Reza Fauzan <reza@icube.us>
2016-09-03 08:29:29 +0700

    edit pelatihan

---
+++ application/modules/admin/controllers/BannerController.php
@@ -0,0 +1,22 @@
+<?php
+class Admin_BannerController extends Zend_Controller_Action {
+    public function indexAction() {
+  		$this->_helper->layout->setLayout('layoutadmin');
+  	}
+
+    public function addAction() {
+      $this->_helper->layout->setLayout('layoutadmin');
+
+    }
+
+    public function editAction() {
+      $this->_helper->layout->setLayout('layoutadmin');
+
+    }
+
+    public function delbannerAction() {
+
+    }
+
+
+}
